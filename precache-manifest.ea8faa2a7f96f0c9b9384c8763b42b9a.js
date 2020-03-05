e2e0566b7639f7af7ffb57693cab314ac3a1ef61
Kiran Pradhan <kpradhan@uw.edu>
2020-03-04 16:57:33 -0800

    Updates

---
+++ precache-manifest.ea8faa2a7f96f0c9b9384c8763b42b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9282877f4fbeec746f6774499fe0a3b",
+    "url": "/project-kpdebate/index.html"
+  },
+  {
+    "revision": "d11de21c8c747ad7706b",
+    "url": "/project-kpdebate/static/css/main.25515226.chunk.css"
+  },
+  {
+    "revision": "b79e4e61b767ca773b01",
+    "url": "/project-kpdebate/static/js/2.58613782.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/project-kpdebate/static/js/2.58613782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11de21c8c747ad7706b",
+    "url": "/project-kpdebate/static/js/main.9146f84c.chunk.js"
+  },
+  {
+    "revision": "e01d9acbe06853859f06",
+    "url": "/project-kpdebate/static/js/runtime-main.762c1fd0.js"
+  }
+]);

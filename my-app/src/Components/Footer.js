e9f5f1fd57ce7a3e923a093cc2a852c9ec19897d
Patrin <patrin.sinteppadon@gmail.com>
2020-03-16 00:28:44 -0700

    comment out the broken link in footer

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component {
       <footer>
           <div className="d-flex justify-content-between align-items-center">
               <div>
-                  <p className="mb-0">Read up on why we made this tool<a href="proposal.html">here.</a></p>
+                  {/* <p className="mb-0">Read up on why we made this tool<a href="proposal.html">here.</a></p> */}
                   <p className="mb-0">&copy; 2020 Kiran Pradhan and Patrin Sinteppadon</p>
               </div>
               <a href="https://github.com/info340a-w20/project-kpdebate">

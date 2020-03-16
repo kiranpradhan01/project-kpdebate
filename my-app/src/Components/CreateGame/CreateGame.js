5f530dcb3bbcf0961b542d41ad5d4a6950d18209
Kiran Pradhan <kpradhan@uw.edu>
2020-03-15 18:23:46 -0700

    Set create game values in firebase

--- my-app/src/Components/CreateGame/CreateGame.js
+++ my-app/src/Components/CreateGame/CreateGame.js
@@ -9,35 +9,35 @@ class CreateGame extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            topic: false,
-            player1: false,
-            player2: false
+            topic: "",
+            player1: "",
+            player2: ""
         }
         let sessionsRef = firebase.database().ref('sessions');
         // generate and lift up random sessionID
         /* TODO: check if randomID is not so random and already exists in firebase */
+        let existingSesssions = firebase.database().ref('sessions');
+        existingSesssions.on('value', (snapshot) => {
+            
+        })
+
         let sessionID = Math.floor(1000 + Math.random() * 9000);
         this.props.updateGame("sessionID", sessionID);
         this.sessionRef = firebase.database().ref('sessions/' + sessionID);
     }
 
-    // firebase testing
-    // 
-    // let session = firebase.database().ref('sessions/2222');
-    // session.set({"topic": "cats or dogs", "player1": "Kiran", "player2": "Patrin"});
-    
     onChange = (key, value) => {
-        if (value !== "") {
-            this.setState({
-                [key]: true // update state to toggle button disabled
-            });
-        } else {
-            this.setState({
-                [key]: false // update state to toggle button disabled
-            });
-        }
+        this.setState({ [key]: value });
         this.props.updateGame(key, value);
-        this.sessionRef.set({[key]: value});
+    }
+
+    updateFirebase = () => {
+        this.sessionRef.set({
+            "topic": this.state.topic,
+            "player1": this.state.player1,
+            "player2": this.state.player2
+        });
+        console.log("updated firebase");
     }
 
     render() {
@@ -47,7 +47,8 @@ class CreateGame extends React.Component {
                 <InputPlayers onInput={this.onChange}/>
                 <section class="createGameContainer">
                     <div class="text-center">
-                        <Link to="/admin-game"><button id="btn-start" class="btn btn-primary btn-lg mt-2 mx-auto" disabled={this.state.topic === false || this.state.player1 === false || this.state.player2 === false}>Begin Debate</button></Link>
+                        <Link to="/admin-game"><button id="btn-start" class="btn btn-primary btn-lg mt-2 mx-auto" disabled={this.state.topic === "" || this.state.player1 === "" || this.state.player2 === ""}
+                        onClick={this.updateFirebase}>Begin Debate</button></Link>
                     </div>
                 </section>
             </div>
